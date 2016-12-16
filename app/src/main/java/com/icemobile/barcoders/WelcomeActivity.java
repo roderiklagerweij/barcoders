1851394fc8b8a8f647be867f4a948d66fb3757d7
Roderik Lagerweij <roderik@icemobile.com>
2016-12-16 10:58:47 +0100

    Welcome activity

---
+++ app/src/main/java/com/icemobile/barcoders/WelcomeActivity.java
@@ -0,0 +1,30 @@
+package com.icemobile.barcoders;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by roderik.lagerweij on 16/12/16.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        View next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+//                startActivity(new Intent());
+            }
+        });
+
+    }
+
+}
