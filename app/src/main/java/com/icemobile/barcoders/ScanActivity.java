5130ce582616d63b5c24596b7a43e8abf69c980c
Roderik Lagerweij <roderik@icemobile.com>
2016-12-16 13:24:23 +0100

    Scan in succession

--- app/src/main/java/com/icemobile/barcoders/ScanActivity.java
+++ app/src/main/java/com/icemobile/barcoders/ScanActivity.java
@@ -14,60 +14,70 @@ import com.icemobile.barcoders.data.Classwiring;
 import com.icemobile.barcoders.data.domain.Product;
 import com.icemobile.barcoders.data.manager.CategoryMapper;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ScanActivity extends AppCompatActivity {
 
-    private int index = -1;
-    private TextView product1TextView;
-    private TextView product2TextView;
-    private TextView product3TextView;
-    private Button next;
-    private Product product1;
-    private Product product2;
-    private Product product3;
+//    private int index = -1;
+//    private TextView product1TextView;
+//    private TextView product2TextView;
+//    private TextView product3TextView;
+//    private Button next;
+//    private Product product1;
+//    private Product product2;
+//    private Product product3;
+
+    private List<String> barcodes = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_scan);
 
-        startActivity(new Intent(this, AnalyzeActivity.class));
+        Toast.makeText(this, "Please scan your first product", Toast.LENGTH_LONG).show();
+        scanCode();
+//        startActivity(new Intent(this, AnalyzeActivity.class));
 
-        View.OnClickListener scanListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.product1:
-                        index = 1;
-                        break;
-                    case R.id.product2:
-                        index = 2;
-                        break;
-                    case R.id.product3:
-                        index = 3;
-                        break;
-                }
-                new IntentIntegrator(ScanActivity.this).initiateScan();
-            }
-        };
+//        View.OnClickListener scanListener = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                switch (view.getId()) {
+//                    case R.id.product1:
+//                        index = 1;
+//                        break;
+//                    case R.id.product2:
+//                        index = 2;
+//                        break;
+//                    case R.id.product3:
+//                        index = 3;
+//                        break;
+//                }
+//                new IntentIntegrator(ScanActivity.this).initiateScan();
+//            }
+//        };
+//
+//        product1TextView = (TextView) findViewById(R.id.product1);
+//        product2TextView = (TextView) findViewById(R.id.product2);
+//        product3TextView = (TextView) findViewById(R.id.product3);
+//        next = (Button) findViewById(R.id.next);
+//        product1TextView.setOnClickListener(scanListener);
+//        product2TextView.setOnClickListener(scanListener);
+//        product3TextView.setOnClickListener(scanListener);
+//
+//        next.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                getSentences();
+//            }
+//        });
+//        setNextButton();
+    }
 
-        product1TextView = (TextView) findViewById(R.id.product1);
-        product2TextView = (TextView) findViewById(R.id.product2);
-        product3TextView = (TextView) findViewById(R.id.product3);
-        next = (Button) findViewById(R.id.next);
-        product1TextView.setOnClickListener(scanListener);
-        product2TextView.setOnClickListener(scanListener);
-        product3TextView.setOnClickListener(scanListener);
-
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getSentences();
-            }
-        });
-        setNextButton();
+    private void scanCode() {
+        new IntentIntegrator(ScanActivity.this).initiateScan();
+        overridePendingTransition(0, 0);
     }
 
     @Override
@@ -77,38 +87,24 @@ public class ScanActivity extends AppCompatActivity {
             if(result.getContents() == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
-                switch (index) {
-                    case 1:
-                        product1 = Classwiring.getCategoryMapper().getProduct(result.getContents());
-                        product1TextView.setText(product1.getBarcode());
-                        break;
-                    case 2:
-                        product2 = Classwiring.getCategoryMapper().getProduct(result.getContents());
-                        product2TextView.setText(product2.getBarcode());
-                        break;
-                    case 3:
-                        product3 = Classwiring.getCategoryMapper().getProduct(result.getContents());
-                        product3TextView.setText(product2.getBarcode());
-                        break;
+                barcodes.add(result.getContents());
+                if (barcodes.size() == 1) {
+                    Toast.makeText(this, "Please scan your second product", Toast.LENGTH_LONG).show();
+                    scanCode();
+                } else if (barcodes.size() == 2) {
+                    Toast.makeText(this, "Please scan your third and final product", Toast.LENGTH_LONG).show();
+                    scanCode();
+                } else {
+                    analyze();
                 }
-                setNextButton();
-//                Toast.makeText(this, "Scanned: " + result.getContents() + " " + index, Toast.LENGTH_LONG).show();
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-
-    private void getSentences() {
-        CategoryMapper categoryMapper = Classwiring.getCategoryMapper();
-        List<String> sentences = categoryMapper.getSentences(Arrays.asList(product1, product2, product3));
-
-        // start final activity with sentences
-    }
-
-    private void setNextButton() {
-        next.setEnabled(product1 != null && product2 != null && product3 != null);
+    private void analyze() {
+//        List<String> sentences = Classwiring.getCategoryMapper().getSentences();
     }
 
 }
