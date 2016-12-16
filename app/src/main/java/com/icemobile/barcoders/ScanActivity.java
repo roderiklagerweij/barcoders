061eef52c27131f0fc2d6b4312abdad02845a602
Roderik Lagerweij <roderik@icemobile.com>
2016-12-16 13:46:50 +0100

    Portrait scanner

--- app/src/main/java/com/icemobile/barcoders/ScanActivity.java
+++ app/src/main/java/com/icemobile/barcoders/ScanActivity.java
@@ -1,16 +1,19 @@
 package com.icemobile.barcoders;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.Result;
 import com.icemobile.barcoders.data.Classwiring;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import zxing.library.DecodeCallback;
+import zxing.library.ZXingFragment;
 
 public class ScanActivity extends AppCompatActivity {
 
@@ -22,35 +25,47 @@ public class ScanActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scan);
 
         Toast.makeText(this, "Please scan your first product", Toast.LENGTH_LONG).show();
-        scanCode();
-    }
+        final ZXingFragment xf = (ZXingFragment) getSupportFragmentManager().findFragmentById(R.id.scanner);
+        xf.setDecodeCallback(new DecodeCallback(){
 
-    private void scanCode() {
-        new IntentIntegrator(ScanActivity.this).initiateScan();
-        overridePendingTransition(0, 0);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
-            } else {
-                barcodes.add(result.getContents());
-                if (barcodes.size() == 1) {
-                    Toast.makeText(this, "Please scan your second product", Toast.LENGTH_LONG).show();
-                    scanCode();
-                } else if (barcodes.size() == 2) {
-                    Toast.makeText(this, "Please scan your third and final product", Toast.LENGTH_LONG).show();
-                    scanCode();
-                } else if (barcodes.size() == 3){
+            @Override
+            public void handleBarcode(Result result, Bitmap arg1, float arg2) {
+                barcodes.add(result.getText());
+                xf.restartScanningIn(200);
+                if (barcodes.size() == 3) {
                     analyze();
                 }
             }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
+        });
+
+    }
+
+//    private void scanCode() {
+////        new IntentIntegrator(ScanActivity.this).initiateScan();
+//        overridePendingTransition(0, 0);
+//    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+//        if(result != null) {
+//            if(result.getContents() == null) {
+//                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+//            } else {
+//                barcodes.add(result.getContents());
+//                if (barcodes.size() == 1) {
+//                    Toast.makeText(this, "Please scan your second product", Toast.LENGTH_LONG).show();
+//                    scanCode();
+//                } else if (barcodes.size() == 2) {
+//                    Toast.makeText(this, "Please scan your third and final product", Toast.LENGTH_LONG).show();
+//                    scanCode();
+//                } else if (barcodes.size() == 3){
+//                    analyze();
+//                }
+//            }
+//        } else {
+//            super.onActivityResult(requestCode, resultCode, data);
+//        }
     }
 
     private void analyze() {
